06a5275609ab8889ab40407be2a1167c95107c45
Chandan Kumar <mechandangupta@gmail.com>
2015-09-17 11:19:00 +0530

    Views created

---
+++ README.md
@@ -0,0 +1,5 @@
+# my-application
+
+> SAPUI5 application
+
+## Getting Started
---
+++ view/Master.view.js
@@ -0,0 +1,30 @@
+(function() {
+	"use strict";
+
+	sap.ui.jsview("view.Master", {
+
+		/** Specifies the Controller belonging to this View. 
+		 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+		 * @memberOf fioricollaboration.ShellView
+		 */
+		getControllerName: function() {
+			return "view.Master";
+		},
+
+		//#######################################################################################################################################################
+		// VIEW LAYOUT FUNCTIONS
+		//#######################################################################################################################################################
+
+		/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+		 * Since the Controller is given to this method, its event handlers can be attached right away.
+		 * @memberOf fioricollaboration.ShellView
+		 */
+		/*jshint unused: vars */
+		createContent: function(oController) {
+			return new sap.m.Page({
+				title: "Products",
+				content: []
+			});
+		}
+	});
+}());
